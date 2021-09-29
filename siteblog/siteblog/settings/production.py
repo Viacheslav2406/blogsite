f05c7fec793ba19c95f612fb172aeec416f9b625
Viacheslav Bakalo <v.bakalo24@gmail.com>
2021-09-29 21:24:26 +0300

    heroku settings

--- siteblog/siteblog/settings/production.py
+++ siteblog/siteblog/settings/production.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR/'static'
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATICFILES_DIRS = [BASE_DIR/'siteblog/static',
                     ]
 
